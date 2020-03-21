162394684b9707b17b3e9dbecfb935fcd44a60f0
m4ik7i <m4ik7i@gmail.com>
2020-03-22 02:13:14 +0900

    refactor: sandbox-fizzbuzz-rust

--- sandbox-fizzbuzz-rust/src/main.rs
+++ sandbox-fizzbuzz-rust/src/main.rs
@@ -1,9 +1,12 @@
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+use std::env;
 
 fn main() -> Result<()> {
-    let n = std::env::args().nth(1).ok_or("None")?.parse::<u128>()?;
+    let n = env::args().nth(1).unwrap().parse::<u128>()?;
 
-    let _ = (1..n + 1).for_each(|i| {
+    (1..=n).for_each(|i| {
         println!("{}", fizzbuzz(i));
     });
 
@@ -12,9 +15,9 @@ fn main() -> Result<()> {
 
 fn fizzbuzz(number: u128) -> String {
     match number {
-        n if n % 3 == 0 && n % 5 != 0 => "Fizz".to_string(),
-        n if n % 5 == 0 && n % 3 != 0 => "Buzz".to_string(),
-        n if n % 3 == 0 && n % 5 == 0 => "FizzBuzz".to_string(),
+        n if n % 3 == 0 && n % 5 != 0 => "Fizz".to_owned(),
+        n if n % 5 == 0 && n % 3 != 0 => "Buzz".to_owned(),
+        n if n % 3 == 0 && n % 5 == 0 => "FizzBuzz".to_owned(),
         n => n.to_string(),
     }
 }
